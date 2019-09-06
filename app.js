9019f5cfa6835e26440a517bec8aa42f0e312b7a
JenniD5 <elizabethduran0105@gmail.com>
2019-09-06 12:57:05 -0700

    id, class, 6/9/19

---
+++ app.js
@@ -0,0 +1,14 @@
+const doge = document.getElementById('doge');
+
+doge.addEventListener('click', ()=>{
+    console.log('Guau, guau.');
+});
+
+const drumButtons =document.querySelectorAll (`button`);
+drumButtons.forEach()
+    { 
+        button=>{buttoon.style.left=button 
+
+        buttoon.style.top= }
+    }
+)
